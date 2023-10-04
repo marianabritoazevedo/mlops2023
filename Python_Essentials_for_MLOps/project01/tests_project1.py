9d544367d70570526ab32d4e6c3bc17d5c6eb093
Mariana Azevedo <mari.brito.azevedo@gmail.com>
2023-10-04 16:54:21 -0300

    #11 Tests projects 1 and 3

--- Python_Essentials_for_MLOps/project01/tests_project1.py
+++ Python_Essentials_for_MLOps/project01/tests_project1.py
@@ -1,18 +1,38 @@
-from project1 import filter_movie_by_id, read_file
+import pytest
+from project1 import *
 
-def test_verify_columns_dataset_movies():
-    movies = read_file('files/movies.csv')
-    columns = list(movies.columns)
+# Fixture to load movies data for testing
+@pytest.fixture
+def movies_data():
+    return read_csv_file("files/movies.csv")
+
+# Fixture to load ratings data for testing
+@pytest.fixture
+def ratings_data():
+    return read_csv_file("files/ratings.csv")
+
+def test_verify_columns_dataset_movies(movies_data):
+    columns = list(movies_data.columns)
     assert columns == ['movieId', 'title', 'genres']
 
-def test_verify_columns_dataset_ratings():
-    ratings = read_file('files/ratings.csv')
-    columns = list(ratings.columns)
+def test_verify_columns_dataset_ratings(ratings_data):
+    columns = list(ratings_data.columns)
     assert columns == ['userId', 'movieId', 'rating', 'timestamp']
 
-def test_filter_movie_by_id():
-    movies = read_file('files/movies.csv')
+def test_clean_title():
+    assert clean_title("The Dark Knight (2008)") == "The Dark Knight 2008"
+
+def test_filter_movie_by_id(movies_data):
     id_movie = 89745
-    movie = filter_movie_by_id(id_movie, movies)
+    movie = filter_movie_by_id(id_movie, movies_data)
     assert movie.title.values[0] == 'Avengers, The (2012)'
 
+def test_search_similar_movies_ratings(movies_data, ratings_data):
+    similar_movies = search_similar_movies_ratings(1, ratings_data, movies_data)
+    assert len(similar_movies) == 10  # Assuming there are 10 similar movies for the given movie ID
+
+# Parameterized test to check if movie titles are cleaned properly
+@pytest.mark.parametrize("input_title, expected_output", [("Inception (2010)", "Inception 2010"),
+                                                         ("Interstellar (2014)", "Interstellar 2014")])
+def test_clean_title_parameterized(input_title, expected_output):
+    assert clean_title(input_title) == expected_output
--- Python_Essentials_for_MLOps/project03/tests_project3.py
+++ Python_Essentials_for_MLOps/project03/tests_project3.py
@@ -1,1 +1,43 @@
-# To do
+import pytest
+from project3 import * 
+
+# Fixture para fornecer dados de amostra para os testes
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame({
+        'Age': [30, 40, 50],
+        'Sex': ['M', 'F', 'M'],
+        'ChestPainType': ['ATA', 'NAP', 'ASY'],
+        'HeartDisease': [0, 1, 1]
+    })
+
+# Testa se a função read_csv_file lê corretamente um arquivo CSV
+def test_read_csv_file(sample_data):
+    file_path = './files/heart_disease_prediction.csv'
+    sample_data.to_csv(file_path, index=False)
+    data = read_csv_file(file_path)
+    assert data.equals(sample_data)
+
+# Testa se a função display_dataframe exibe corretamente informações sobre o DataFrame
+def test_display_dataframe(capsys, sample_data):
+    display_dataframe(sample_data)
+    captured = capsys.readouterr()
+    assert "Age" in captured.out
+
+# Testa se a função column_types_and_counts exibe corretamente os tipos e contagens de colunas
+def test_column_types_and_counts(capsys, sample_data):
+    column_types_and_counts(sample_data)
+    captured = capsys.readouterr()
+    assert "int64" in captured.out and "object" in captured.out
+
+# Testa se a função show_unique_values exibe corretamente os valores únicos das colunas especificadas
+def test_show_unique_values(capsys, sample_data):
+    show_unique_values(['Sex', 'ChestPainType'], sample_data)
+    captured = capsys.readouterr()
+    assert "Column Sex:" in captured.out
+
+# Testa se a função separate_features_label separa corretamente as características e rótulos
+def test_separate_features_label(sample_data):
+    X, y = separate_features_label(sample_data)
+    assert list(X.columns) == ['Age', 'Sex', 'ChestPainType']
+    assert list(y) == [0, 1, 1]
