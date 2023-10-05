0f5f0613051e9892f6993f4cc521e6bed0fc8db3
Mariana Azevedo <mari.brito.azevedo@gmail.com>
2023-10-05 12:51:29 -0300

    #13 update project2

--- Python_Essentials_for_MLOps/project02/dags/project2.py
+++ Python_Essentials_for_MLOps/project02/dags/project2.py
@@ -1,26 +1,22 @@
 import os
 import json
+import logging
 import requests
+import pendulum
 import xmltodict
+from pydub import AudioSegment
+from vosk import Model, KaldiRecognizer
 
 from airflow.decorators import dag, task
-import pendulum
-from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 
 PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
 EPISODE_FOLDER = "episodes"
+FRAME_RATE = 16000
 
-@dag(
-    dag_id='podcast_summary',
-    schedule_interval="@daily",
-    start_date=pendulum.datetime(2022, 5, 30),
-    catchup=False,
-)
-def podcast_summary():
-    create_database = SqliteOperator(
-        task_id='create_table_sqlite',
-        sql=r"""
+def create_database():
+    create_table_sql = """
         CREATE TABLE IF NOT EXISTS episodes (
             link TEXT PRIMARY KEY,
             title TEXT,
@@ -29,47 +25,163 @@ def podcast_summary():
             description TEXT,
             transcript TEXT
         );
-        """,
+    """
+    create_database = SqliteOperator(
+        task_id='create_table_sqlite',
+        sql=create_table_sql,
         sqlite_conn_id="podcasts"
     )
+    return create_database
 
-    @task()
-    def get_episodes():
-        data = requests.get(PODCAST_URL)
-        feed = xmltodict.parse(data.text)
+@task()
+def get_episodes():
+    '''
+    This task retrieves the latest episodes from
+    the PODCAST_URL and returns them as a list.
+    
+    Returns:
+        list: List of latest podcast episodes.
+    '''
+    try:
+        response = requests.get(PODCAST_URL, timeout=10)
+        response.raise_for_status()  # Raise HTTPError for bad requests
+
+        feed = xmltodict.parse(response.text)
         episodes = feed["rss"]["channel"]["item"]
-        print(f"Found {len(episodes)} episodes.")
+        num_episodes = len(episodes)
+        logging.info("ℹ️ Found %s episodes.", num_episodes)
         return episodes
 
-    podcast_episodes = get_episodes()
-    create_database.set_downstream(podcast_episodes)
+    except requests.exceptions.HTTPError as http_err:
+        logging.error("HTTP error occurred: %s", http_err)
+        raise
+    except Exception as err:
+        logging.error("An error occurred: %s", err)
+        raise
 
-    @task()
-    def load_episodes(episodes):
+@task()
+def load_episodes(episodes):
+    '''
+    This task loads new episodes into the SQLite database.
+    
+    Args:
+        episodes (list): List of new episodes to be loaded.
+        
+    Returns:
+        list: List of episodes successfully loaded into the database.
+    '''
+    try:
         hook = SqliteHook(sqlite_conn_id="podcasts")
         stored_episodes = hook.get_pandas_df("SELECT * from episodes;")
         new_episodes = []
+
         for episode in episodes:
             if episode["link"] not in stored_episodes["link"].values:
                 filename = f"{episode['link'].split('/')[-1]}.mp3"
-                new_episodes.append([episode["link"], episode["title"], episode["pubDate"], episode["description"], filename])
+                new_episodes.append([episode["link"], episode["title"],
+                episode["pubDate"], episode["description"], filename])
 
-        hook.insert_rows(table='episodes', rows=new_episodes, target_fields=["link", "title", "published", "description", "filename"])
+        hook.insert_rows(table='episodes', rows=new_episodes,
+        target_fields=["link", "title", "published", "description", "filename"])
+        logging.info("✅ Loaded %s new episodes into the database.", len(new_episodes))
+        return new_episodes
+    except Exception as err:
+        logging.error("❌ An error occurred while loading episodes: %s", err)
+        raise
 
-    load_episodes(podcast_episodes)
+@task()
+def download_episodes(episodes):
+    '''
+    This task loads new episodes into the SQLite database.
+    
+    Args:
+        episodes (list): List of new episodes to be loaded.
+        
+    Returns:
+        list: List of episodes successfully loaded into the database.
+    '''
+    try:
+        hook = SqliteHook(sqlite_conn_id="podcasts")
+        stored_episodes = hook.get_pandas_df("SELECT * from episodes;")
+        new_episodes = []
 
-    @task()
-    def download_episodes(episodes):
         for episode in episodes:
-            name_end = episode["link"].split('/')[-1]
-            filename = f"{name_end}.mp3"
-            audio_path = os.path.join(EPISODE_FOLDER, filename)
-            if not os.path.exists(audio_path):
-                print(f"Downloading {filename}")
-                audio = requests.get(episode["enclosure"]["@url"])
-                with open(audio_path, "wb+") as f:
-                    f.write(audio.content)
+            if episode["link"] not in stored_episodes["link"].values:
+                filename = f"{episode['link'].split('/')[-1]}.mp3"
+                new_episodes.append([episode["link"], episode["title"],
+                episode["pubDate"], episode["description"], filename])
 
-    download_episodes(podcast_episodes)
+        hook.insert_rows(table='episodes', rows=new_episodes,
+        target_fields=["link", "title", "published", "description", "filename"])
+        logging.info("✅ Loaded %s new episodes into the database.", len(new_episodes))
+        return new_episodes
+    except Exception as err:
+        logging.error("❌ An error occurred while loading episodes: %s", err)
+        raise
 
-summary = podcast_summary()
+@task()
+def speech_to_text(audio_files, new_episodes):
+    '''
+    This task transcribes audio files into text and updates the database with the transcripts.
+    
+    Args:
+        audio_files (list): List of audio file information (e.g., file paths).
+        new_episodes (list): List of new episodes to be transcribed.
+        
+    Returns:
+        int: Number of episodes successfully transcribed and updated in the database.
+    '''
+    try:
+        hook = SqliteHook(sqlite_conn_id="podcasts")
+        transcribed_count = 0
+
+        model = Model(model_name="vosk-model-en-us-0.22-lgraph")
+        rec = KaldiRecognizer(model, FRAME_RATE)
+        rec.SetWords(True)
+
+        for episode_info in new_episodes:
+            episode_link = episode_info["link"]
+            filename = episode_info["filename"]
+
+            if any(entry["filename"] == filename for entry in audio_files):
+                print(f"Transcribing {filename}")
+                filepath = os.path.join(EPISODE_FOLDER, filename)
+                mp3 = AudioSegment.from_mp3(filepath)
+                mp3 = mp3.set_channels(1)
+                mp3 = mp3.set_frame_rate(FRAME_RATE)
+
+                step = 20000
+                transcript = ""
+                for i in range(0, len(mp3), step):
+                    segment = mp3[i:i+step]
+                    rec.AcceptWaveform(segment.raw_data)
+                    result = rec.Result()
+                    text = json.loads(result)["text"]
+                    transcript += text
+
+                # Update the database with the transcript
+                hook.run("UPDATE episodes SET transcript=? WHERE link=?",
+                (transcript, episode_link))
+                transcribed_count += 1
+        logging.info("✅ Transcribed %s episodes.", transcribed_count)
+        return transcribed_count
+    except Exception as err:
+        logging.error("❌ An error occurred while transcribing episodes: %s", err)
+        raise
+
+@dag(
+    dag_id='podcast_summary3',
+    schedule_interval="@daily",
+    start_date=pendulum.datetime(2022, 5, 30),
+    catchup=False,
+)
+def podcast_summary3():
+    create_database_task = create_database()
+    episodes = get_episodes()
+    new_episodes = load_episodes(episodes)
+    audio_files = download_episodes(episodes)
+    transcribed_count = speech_to_text(audio_files, new_episodes)
+
+    create_database_task >> episodes >> new_episodes >> audio_files >> transcribed_count
+
+SUMMARY = podcast_summary3()
